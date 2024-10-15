0cd0478ab08e65be9d3168f0952955ebce7832a3
crcristian97 <crcristian97.cc@gmail.com>
2024-10-15 09:55:17 -0300

    se eliminan dependencias

--- app/components/LayoutGridProject.tsx
+++ app/components/LayoutGridProject.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { LayoutGrid } from "./ui/layout-grid";
 import Academic from "../public/AcademyDigital.png";
 import EmailVerification from "../public/emailVerification.png";
